2a40e8e34e97be58b65a18f3c1b35c49ab43e93a
Kaiserfires <the00kaiser@gmail.com>
2024-05-12 03:12:15 -0300

    empieza el final

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './componentes/login/login.component';
+import { PrincipalComponent } from './componentes/principal/principal.component';
+import { RegistroComponent } from './componentes/registro/registro.component';
+import { BienvenidaComponent } from './componentes/bienvenida/bienvenida.component';
+import { ErrorComponent } from './componentes/error/error.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'principal',component:PrincipalComponent,
+        children:[
+            {path:'',component:LoginComponent},
+            {path:'registro',component:RegistroComponent},
+            {path:'bienvenida',component:BienvenidaComponent}
+        ]
+    },
+    {path:'',redirectTo:'principal',pathMatch:'full'},
+    {path:'**',component:ErrorComponent}
+];
